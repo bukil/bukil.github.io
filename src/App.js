4dce38ac060386ab5028b9f2698271874f1b7d5c
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2025-08-22 11:46:21 +0530

    text
    text

--- src/App.js
+++ src/App.js
@@ -40,10 +40,9 @@ function App() {
         <ul className="credits-list" aria-label="Contributor list">
           <li><span className="contrib-item single-line">This project was carried out as part of Project 2 at IDC, IIT Bombay, under the guidance of <a className="prof-link" href="https://www.idc.iitb.ac.in/people/phd/girish-vinod-dalvi" target="_blank" rel="noopener noreferrer">Professor Girish Dalvi.</a></span></li>
           <li><span className="contrib-item single-line">Data Geometry <a className="prof-link" href="https://in.linkedin.com/in/sumant-rao-a5aaa042" target="_blank" rel="noopener noreferrer">Professor Sumant Rao.</a></span></li>
-          <li><span className="contrib-item">Professor Sumant Rao– Data Geometry</span></li>
-          <li><span className="contrib-item">xxxxxx – Spectral Models</span></li>
-          <li><span className="contrib-item">xxxxxx – Interaction Design</span></li>
-          <li><span className="contrib-item">xxxxxx – Visualization Engine</span></li>
+          <li><span className="contrib-item">xxxxxx – </span></li>
+          <li><span className="contrib-item">xxxxxx – </span></li>
+          <li><span className="contrib-item">xxxxxx – </span></li>
         </ul>
       </section>
   <Footer />
